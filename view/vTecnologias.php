75a7c194d78f76f7e2e6012ac391c56897236778
David <David.pralos@educa.jcyl.es>
2022-03-03 12:37:01 +0100

    tecnologias

--- view/vTecnologias.php
+++ view/vTecnologias.php
@@ -6,7 +6,8 @@
     </form>
 </header>
 <div class="wikiTecnologias">
-    <a href="https://es.wikipedia.org/wiki/HTML" target="_blank"><img src="img/html.png" width="300px" height="300px"></img></a>
-    <a href="https://es.wikipedia.org/wiki/CSS" target="_blank"><img src="img/css.png" width="300px" height="300px"></a>
-    <a href="https://es.wikipedia.org/wiki/PHP" target="_blank"><img src="img/php.png" width="300px" height="300px"></a>
+    <a href="https://es.wikipedia.org/wiki/HTML" target="_blank"><img src="img/html.png" width="200px" height="200px"></img></a>
+    <a href="https://es.wikipedia.org/wiki/CSS" target="_blank"><img src="img/css.png" width="200px" height="200px"></a>
+    <a href="https://es.wikipedia.org/wiki/PHP" target="_blank"><img src="img/php.png" width="200px" height="200px"></a>
+    <a href="https://es.wikipedia.org/wiki/MySQL" target="_blank"><img src="img/mysql.png" width="200px" height="200px"></a>
 </div>
